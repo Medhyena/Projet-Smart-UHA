d20ac2cd1a8c222ef766e8d2160fbf855769ff1b
Daniel Wettel <daniel.wettel@uha.fr>
2018-12-17 16:28:17 +0100

    fix unmerged files

--- optimisation/OptiAlgo.js
+++ optimisation/OptiAlgo.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var wslibrary = require("ws");
 var points_et_colis;
 // Fonction d'optimisation naïve
@@ -26,36 +25,6 @@ function connexionServeurWebSocket(ip_du_serveur, port_du_serveur) {
     var vehicule_id = [false, true, true, false]; // véhicule disponible = true, non disponible = false
     ws.send(JSON.stringify(optimisationNaive(vehicule_id)));
   });
-=======
-var wslibrary = require('ws');
-var points_et_colis;
-// Fonction d'optimisation naïve
-function optimisationNaive(vehicule_id) {
-    var vehicule_et_points_et_colis = new Array(4); // nombre de véhicules
-    var i;
-    for (i = 0; vehicule_id[i] != true; i++)
-        ;
-    console.log(points_et_colis);
-    vehicule_et_points_et_colis[i] = points_et_colis.slice();
-    vehicule_id[i] = false;
-    console.log(vehicule_et_points_et_colis);
-    return vehicule_et_points_et_colis;
-}
-// Fonction préparant la connexion WebSocket
-function connexionServeurWebSocket(ip_du_serveur, port_du_serveur) {
-    var ws = new wslibrary('ws://' + ip_du_serveur + ':' + port_du_serveur + '/');
-    console.log('ws://' + ip_du_serveur + ':' + port_du_serveur + '/');
-    ws.on('open', function open() {
-        console.log("Succesfully connected!");
-    });
-    // Fonction exécutée quand on reçoit un message
-    ws.on('message', function incoming(data) {
-        console.log(data);
-        points_et_colis = data;
-        var vehicule_id = [false, true, true, false]; // véhicule disponible = true, non disponible = false
-        ws.send(JSON.stringify(optimisationNaive(vehicule_id)));
-    });
->>>>>>> 89c9dca61c801ea3518df837e63cc5571a6e5d34
 }
 // Il faut mettre l'IP du serveur ici
 var ip_du_serveur = "localhost";
--- optimisation/OptiAlgo.ts
+++ optimisation/OptiAlgo.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 declare function require(name: string);
 const wslibrary = require("ws");
 
@@ -36,43 +35,6 @@ function connexionServeurWebSocket(
   });
 }
 
-=======
-declare function require(name:string);
-const wslibrary = require('ws');
-
-var points_et_colis:number[][][];
-
-// Fonction d'optimisation naïve
-function optimisationNaive(vehicule_id:boolean[]): number[][][][] {
-    let vehicule_et_points_et_colis:number[][][][] = new Array(4); // nombre de véhicules
-    let i:number;
-    for (i = 0; vehicule_id[i] != true; i++);
-    console.log(points_et_colis);
-    vehicule_et_points_et_colis[i] = points_et_colis.slice();
-    vehicule_id[i] = false;
-    console.log(vehicule_et_points_et_colis);
-    return vehicule_et_points_et_colis;
-}
-
-// Fonction préparant la connexion WebSocket
-function connexionServeurWebSocket(ip_du_serveur:String, port_du_serveur:String)
-{
-    let ws = new wslibrary('ws://' + ip_du_serveur + ':' + port_du_serveur + '/');
-    console.log('ws://' + ip_du_serveur + ':' + port_du_serveur + '/');
-    ws.on('open', function open() {
-        console.log("Succesfully connected!");
-    });
-
-    // Fonction exécutée quand on reçoit un message
-    ws.on('message', function incoming(data) {
-        console.log(data);
-        points_et_colis = data;
-        let vehicule_id:boolean[] = [false, true, true, false]; // véhicule disponible = true, non disponible = false
-        ws.send(JSON.stringify(optimisationNaive(vehicule_id)));
-    });
-}
-
->>>>>>> 89c9dca61c801ea3518df837e63cc5571a6e5d34
 // Il faut mettre l'IP du serveur ici
 var ip_du_serveur = "localhost";
 // Il faut mettre le port sur lequel écoute le serveur ici
